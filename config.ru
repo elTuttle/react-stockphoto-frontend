fb9bd8d53515e9d57fccf0f6a2b7f4afb0e3b0a4
github username <github email address>
2018-01-03 15:55:11 -0600

    CORS3

--- config.ru
+++ config.ru
@@ -2,21 +2,4 @@
 
 require_relative 'config/environment'
 
-use Rack::Cors do
-  allow do
-    origins 'localhost:3000', '127.0.0.1:3000'
-            # regular expressions can be used here
-
-    resource '*', :headers => 'x-domain-token'
-    resource '*',
-        :methods => [:get, :post, :put, :delete, :options]
-        # headers to expose
-  end
-
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => :get
-  end
-end
-
 run Rails.application
--- config/application.rb
+++ config/application.rb
@@ -14,14 +14,7 @@ module StockphotoContest
     config.to_prepare do
       DeviseController.respond_to :html, :json
     end
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
